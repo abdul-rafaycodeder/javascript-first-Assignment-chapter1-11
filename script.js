3c8730f39f077ecda4f903c1cf7962bdd792ae14
abdul rafay <abdulrafaycodeder@email.com>
2025-12-07 08:10:13 -0800

    js

---
+++ script.js
@@ -0,0 +1,79 @@
+
+// ----------alert----------- //
+// 1.Write an alert that shows <Hello, World!=
+// alert("Hello, World");
+
+// 2.Show an alert with your name.
+// alert("abdul rafay");
+
+// 3.Show a number using alert().
+// alert("5");
+
+// 4.Store a message in a variable and show it in an alert
+// var uname = "abdul rafay";
+// alert(uname);
+
+// 5.Combine two strings and display them in an alert.
+// var uname = "osama";
+// var newName = "rafay + uname;
+// alert(newName)
+
+
+
+// -------Variables for Strings------- //
+
+// 1.Declare a variable userName and assign your name to it
+// var userName = "abdul rafay";
+
+// 2.Store a welcome message in a variable and alert() it.
+// var message = "welcome";
+// alert(welcome)
+
+// 3.Combine first name and last name into one variable.
+// var firstName = "abdul"
+// var lastName = "rafay"
+// var fullName = firstName + lastName;
+
+// 4.Store your favorite quote in a variable.
+// var favoriteQuote = "Success comes to those who work hard.";
+
+// 5.Create a variable city and show <Welcome to [city]!= in an alert
+// var city = "Welcome to [city]"
+// alert(city)
+
+
+// -------Variables for Numbers------- //
+// 1.Declare a variable age and assign your age to it.
+// var age = 17;
+
+// 2.Store two numbers in variables and add them.
+// var num1 = 5;
+// var num2 = 12;
+// var sum = num1 + num2;
+
+// 3.Subtract one number from another.
+// var num1 = 5;
+// var num2 = 12;
+// var result = num1 - num2;
+// console.log(result)
+
+// 4.Multiply two numbers stored in variables.
+// var num1 = 5;
+// var num2 = 30;
+// var result = num1 * num2;
+// alert(result);
+
+// 5.Show a message like: <You are 20 years old.=
+// alert("<You are 20 years old.=")
+
+
+// -------Math Expressions------- //
+
+// 1.Create two variables: x = 10, y = 5 4 add them and 
+// var x = 10;
+// var y = 5;
+// var sum = x + y;
+// alert(sum);
+
+// 2.
+// 
